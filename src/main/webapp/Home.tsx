2e7ddcc7ecfee678fbcf455ca615a0feb94e2ae0
andreaschiona <andrea82>
2019-05-05 11:30:15 +0200

    First success call backend api

--- src/main/webapp/Home.tsx
+++ src/main/webapp/Home.tsx
@@ -1,16 +1,21 @@
 import app, {Component} from 'apprun';
 
 export default class HomeComponent extends Component {
-  state = 'Home';
+  state = {};
 
   view = (state) => {
-    return <div>
-      <h1>{state}</h1>
-    </div>
-  }
+    return <><div><button $onclick='fetchTodo'>Refresh</button></div>
+      {state.todolist && 
+        <h3>{state.todolist[0].name}</h3> }
+      </>};
 
   update = {
-    '#Home': state => state,
-  }
+    '#Home': async state => {
+      const response = await fetch('/api/todo/');
+      const todolist = await response.json();
+      return {todolist};
+    }
+
+  };
 }
 
