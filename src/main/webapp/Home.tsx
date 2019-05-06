b40cc03e8ab4e4e7b0005265e2fefc3eec969b04
andreaschiona <andrea_schiona@mytria.it>
2019-05-06 18:07:49 +0200

    start to do new modal form for insert new todo

--- src/main/webapp/Home.tsx
+++ src/main/webapp/Home.tsx
@@ -16,6 +16,9 @@ export default class HomeComponent extends Component {
 
   view = (state) => {
     return <>
+      <div>
+        <a href="" class="btn btn-primary" data-toggle="modal" data-target="#modalNewForm">New</a>
+      </div>
       {state.todolist ? 
         <table class="table">
           <thead>
@@ -36,7 +39,41 @@ export default class HomeComponent extends Component {
         </table>
       :
         <div className="article-preview">No articles are here... yet.</div>}
-      </>};
+
+<div class="modal fade" id="modalNewForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header text-center">
+        <h4 class="modal-title w-100 font-weight-bold">Create new Todo</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body mx-3">
+        <form onsubmit={e => this.run('submit-new-todo', e)}>
+          <div class="md-form mb-5">
+            <i class="fas fa-envelope prefix grey-text"></i>
+            <input type="string" id="name" class="form-control validate" />
+            <label data-error="wrong" data-success="right" for="name">Todo name</label>
+          </div>
+          <div class="md-form mb-5">
+            <i class="fas fa-envelope prefix grey-text"></i>
+            <input type="string" id="description" class="form-control validate" />
+            <label data-error="wrong" data-success="right" for="description">Todo description</label>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <button class="btn btn-default" $onclick='saveTodo'>Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+</>
+};
+
+
 
   update = {
     '#': async state => {
@@ -47,6 +84,84 @@ export default class HomeComponent extends Component {
       const response = await fetch('/api/todo/');
       const todolist = await response.json();
       return {todolist};
+    },
+
+    'saveTodo': async state => {
+      const name = document.getElementById('name') as HTMLInputElement;
+      const description = document.getElementById('description') as HTMLInputElement;
+    },
+
+    // function serializeObject<T>(form) {
+    //   let obj = {};
+    //   if (typeof form == 'object' && form.nodeName == "FORM") {
+    //     for (let i = 0; i < form.elements.length; i++) {
+    //       const field = form.elements[i];
+    //       if (field.name
+    //         && field.type != 'file'
+    //         && field.type != 'reset'
+    //         && field.type != 'submit'
+    //         && field.type != 'button') {
+    //         if (field.type == 'select-multiple') {
+    //           obj[field.name] = '';
+    //           let tempvalue = '';
+    //           for (let j = 0; j < form.elements[i].options.length; j++) {
+    //             if (field.options[j].selected)
+    //               tempvalue += field.options[j].value + ';';
+    //           }
+    //           if (tempvalue.charAt(tempvalue.length - 1) === ';') obj[field.name] = tempvalue.substring(0, tempvalue.length - 1);
+    //         } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
+    //           obj[field.name] = field.value;
+    //         }
+    //       }
+    //     }
+    //   }
+    //   return obj as T;
+    // },
+
+    'submit-new-todo': (state, e) => {
+      try {
+        e.preventDefault();
+
+        let obj = {};
+        let form = e.target;
+      if (typeof form == 'object' && form.nodeName == "FORM") {
+        for (let i = 0; i < form.elements.length; i++) {
+          const field = form.elements[i];
+          if (field.name
+            && field.type != 'file'
+            && field.type != 'reset'
+            && field.type != 'submit'
+            && field.type != 'button') {
+            if (field.type == 'select-multiple') {
+              obj[field.name] = '';
+              let tempvalue = '';
+              for (let j = 0; j < form.elements[i].options.length; j++) {
+                if (field.options[j].selected)
+                  tempvalue += field.options[j].value + ';';
+              }
+              if (tempvalue.charAt(tempvalue.length - 1) === ';') obj[field.name] = tempvalue.substring(0, tempvalue.length - 1);
+            } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
+              obj[field.name] = field.value;
+            }
+          }
+        }
+      }
+
+
+        const todo = obj as any; //serializeObject<any>(e.target);
+        const headers = { 'Content-Type': 'application/json; charset=utf-8' };
+        fetch('/api/todo/', {
+          method: 'post',
+          headers: headers,
+          body: todo
+        }).then(function(response) {
+          document.getElementById('name').nodeValue = null;
+          document.getElementById('description').nodeValue = null;
+          return response.json();
+        });
+      } catch ({ errors }) {
+        return { ...state, errors }
+      }
     }
 
   };
