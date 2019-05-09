0de07ae0779c60d76aa8872b798740aa2873f71d
andreaschiona <andrea_schiona@mytria.it>
2019-05-09 13:05:29 +0200

    Fix modal dialog for new todo + clean code

--- src/main/webapp/Home.tsx
+++ src/main/webapp/Home.tsx
@@ -1,7 +1,10 @@
 import app, {Component} from 'apprun';
+declare var $;
 
 export default class HomeComponent extends Component {
-  state = {};
+  state = {
+    todolist: []
+  };
 
   Todo = ({todo}) => (
     <tr id={todo.id} key={todo.id}>
@@ -21,7 +24,6 @@ export default class HomeComponent extends Component {
       <div>
         <a href="" class="btn btn-primary" data-toggle="modal" data-target="#modalNewForm">New</a>
       </div>
-      {state.todolist ? 
         <table class="table">
           <thead>
             <tr>
@@ -39,43 +41,42 @@ export default class HomeComponent extends Component {
             )}
           </tbody>
         </table>
-      :
-        <div className="article-preview">No articles are here... yet.</div>}
+        {state.todolist.size == 0 &&
+          <div className="todo-empty">No todo are here... yet.</div>
+        }
 
-<div class="modal fade" id="modalNewForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-  aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header text-center">
-        <h4 class="modal-title w-100 font-weight-bold">Create new Todo</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body mx-3">
-        <form id="newtodo-form" onsubmit={e => this.run('createNewTodo', e)}>>
-          <div class="md-form mb-5">
-            <i class="fas fa-envelope prefix grey-text"></i>
-            <input type="string" id="name" class="form-control validate" />
-            <label data-error="wrong" data-success="right" for="name">Todo name</label>
+        <div class="modal fade" id="modalNewForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+          aria-hidden='true'>
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header text-center">
+                <h4 class="modal-title w-100 font-weight-bold">Create new Todo</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body mx-3">
+                <form id="newtodo-form" onsubmit={e => this.run('createNewTodo', e)}>
+                  <div class="md-form mb-5">
+                    <i class="fas fa-envelope prefix grey-text"></i>
+                    <input type="string" id="name" class="form-control validate" />
+                    <label data-error="wrong" data-success="right" for="name">Todo name</label>
+                  </div>
+                  <div class="md-form mb-5">
+                    <i class="fas fa-envelope prefix grey-text"></i>
+                    <input type="string" id="description" class="form-control validate" />
+                    <label data-error="wrong" data-success="right" for="description">Todo description</label>
+                  </div>
+                  <div class="modal-footer d-flex justify-content-center">
+                    <button class="btn btn-primary" type="submit" id="save">Save</button>
+                  </div>
+                </form>
+              </div>
+            </div>
           </div>
-          <div class="md-form mb-5">
-            <i class="fas fa-envelope prefix grey-text"></i>
-            <input type="string" id="description" class="form-control validate" />
-            <label data-error="wrong" data-success="right" for="description">Todo description</label>
-          </div>
-          <div class="modal-footer d-flex justify-content-center">
-            <button class="btn btn-default" type="submit" id="save">Save</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
-</>
+        </div>
+      </>
 };
-
-
 
   update = {
     '#': async state => {
@@ -89,7 +90,7 @@ export default class HomeComponent extends Component {
     },
 
     'createNewTodo': async (state, e) => {
-      e.preventDefault();
+      //e.preventDefault();
       let todo = {name: e.target["name"].value, description: e.target["description"].value};
       console.log("Going to POST " + JSON.stringify(todo));    
       fetch("/api/todo/",
@@ -101,84 +102,9 @@ export default class HomeComponent extends Component {
             method: "POST",
             body: JSON.stringify(todo)
         })
-      .then(res=>res.json())
+      .then(res => res.json())
       .then(res => console.log(res))
-      .then(res => {true});
-    },
-
-    // function serializeObject<T>(form) {
-    //   let obj = {};
-    //   if (typeof form == 'object' && form.nodeName == "FORM") {
-    //     for (let i = 0; i < form.elements.length; i++) {
-    //       const field = form.elements[i];
-    //       if (field.name
-    //         && field.type != 'file'
-    //         && field.type != 'reset'
-    //         && field.type != 'submit'
-    //         && field.type != 'button') {
-    //         if (field.type == 'select-multiple') {
-    //           obj[field.name] = '';
-    //           let tempvalue = '';
-    //           for (let j = 0; j < form.elements[i].options.length; j++) {
-    //             if (field.options[j].selected)
-    //               tempvalue += field.options[j].value + ';';
-    //           }
-    //           if (tempvalue.charAt(tempvalue.length - 1) === ';') obj[field.name] = tempvalue.substring(0, tempvalue.length - 1);
-    //         } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
-    //           obj[field.name] = field.value;
-    //         }
-    //       }
-    //     }
-    //   }
-    //   return obj as T;
-    // },
-
-    'submit-new-todo': (state, e) => {
-      try {
-        e.preventDefault();
-
-        let obj = {};
-        let form = e.target;
-      if (typeof form == 'object' && form.nodeName == "FORM") {
-        for (let i = 0; i < form.elements.length; i++) {
-          const field = form.elements[i];
-          if (field.name
-            && field.type != 'file'
-            && field.type != 'reset'
-            && field.type != 'submit'
-            && field.type != 'button') {
-            if (field.type == 'select-multiple') {
-              obj[field.name] = '';
-              let tempvalue = '';
-              for (let j = 0; j < form.elements[i].options.length; j++) {
-                if (field.options[j].selected)
-                  tempvalue += field.options[j].value + ';';
-              }
-              if (tempvalue.charAt(tempvalue.length - 1) === ';') obj[field.name] = tempvalue.substring(0, tempvalue.length - 1);
-            } else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
-              obj[field.name] = field.value;
-            }
-          }
-        }
-      }
-
-
-        const todo = obj as any; //serializeObject<any>(e.target);
-        const headers = { 'Content-Type': 'application/json; charset=utf-8' };
-        fetch('/api/todo/', {
-          method: 'post',
-          headers: headers,
-          body: todo
-        }).then(function(response) {
-          document.getElementById('name').nodeValue = null;
-          document.getElementById('description').nodeValue = null;
-          return response.json();
-        });
-      } catch ({ errors }) {
-        return { ...state, errors }
-      }
     }
-
   };
 }
 
